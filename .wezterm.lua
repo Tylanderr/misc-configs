99d5f004b728951defeee7779c4885da614ac193
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-01 06:41:19 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,9 @@
 local wezterm = require 'wezterm'
-
 local mux = wezterm.mux
 
 wezterm.on("gui-startup", function()
-  local tab, pane, window = mux.spawn_window{}
-  window:gui_window():maximize()
+  -- local tab, pane, window = mux.spawn_window{}
+  -- window:gui_window():maximize()
 end)
 
 wezterm.on('format-window-title', function ()
@@ -18,17 +17,14 @@ config.check_for_updates = false
 config.audible_bell = "Disabled"
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
-config.font_size = 11
+config.font_size = 10
 config.term = "xterm-256color"
 config.enable_tab_bar = false
 
-config.window_decorations = "RESIZE"
-config.window_background_image_hsb = {
-	brightness = .02
-}
-
-local background = 'C:\\Documents\\TermBG\\02.jpg'
 local opacity = 1
+--local background = 'C:\\Documents\\TermBG\\02.jpg'
+--config.window_background_image_hsb = { brightness = .003 }
+config.window_decorations = "RESIZE"
 config.window_background_opacity = opacity
 config.window_background_image = background
 
@@ -45,6 +41,7 @@ wezterm.on('toggle-background', function(window, pane)
   end
   window:set_config_overrides(overrides)
 end)
+
 
 wezterm.on('toggle-opacity', function(window, pane)
   local overrides = window:get_config_overrides() or {}
