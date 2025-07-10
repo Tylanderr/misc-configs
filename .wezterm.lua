3eb9064b0e0c24e5fcb226d988828b9fe31e2da1
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-10 07:37:18 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 
--- wezterm.on("gui-startup", function()
---   local tab, pane, window = mux.spawn_window{}
---   window:gui_window():maximize()
--- end)
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 local config = wezterm.config_builder()
 config.default_domain = 'WSL:Ubuntu'
@@ -16,23 +16,26 @@ config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
 config.font_size = 10
 config.term = "xterm-256color"
 config.enable_tab_bar = false
-
-config.window_background_image_hsb = { brightness = .005 }
-local background = 'C:\\Documents\\TermBG\\07.jpg'
-local opacity = 1
-
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
+
+local opacity = 1
+local background = 'C:\\Documents\\TermBG\\02.jpg'
+config.window_background_image_hsb = { brightness = .0025 }
 config.window_background_opacity = opacity
+config.window_background_image = background
 
 wezterm.on('toggle-background', function(window, pane)
-    local overrides = window:get_config_overrides() or {}
-    if not overrides.window_background_image then
-        overrides.window_background_image = background
-    else
-        overrides.window_background_image = nil
-    end
-    window:set_config_overrides(overrides)
+  local overrides = window:get_config_overrides() or {}
+  local current = overrides.window_background_image
+  if current == nil then
+    overrides.window_background_image = ''
+  elseif current == '' then
+    overrides.window_background_image = background_image
+  else
+    overrides.window_background_image = ''
+  end
+  window:set_config_overrides(overrides)
 end)
 
 wezterm.on('toggle-opacity', function(window, pane)
@@ -57,15 +60,10 @@ config.keys = {
     action = wezterm.action.DisableDefaultAssignment,
   },
   {
-    key = 'B',
-    mods = 'CTRL',
-    action = wezterm.action.EmitEvent 'toggle-background',
-  },
-  {
     key = 'O',
     mods = 'CTRL',
-    action = wezterm.action.EmitEvent 'toggle-opacity',
-  },
+    action = wezterm.action.EmitEvent 'toggle-background',
+  }
 }
 
 return config
