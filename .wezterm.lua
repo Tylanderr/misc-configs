99f429c274210537068c1a3286bf8116cf3c618a
Tyler Harding <tyler.harding.sde@gmail.com>
2025-07-22 09:37:26 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,6 +1,11 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
+
 local config = wezterm.config_builder()
 config.default_domain = 'WSL:Ubuntu'
 config.check_for_updates = false
@@ -8,20 +13,24 @@ config.check_for_updates = false
 config.audible_bell = "Disabled"
 config.color_scheme = 'Tokyo Night'
 config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
-config.font_size = 10
+config.font_size = 9
 config.term = "xterm-256color"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 
-config.window_background_opacity = 1
+local background = 'C:\\Documents\\WezTerm\\background.jpg'
+config.window_background_image = background
 
-wezterm.on('toggle-opacity', function(window, pane)
+wezterm.on('toggle-background', function(window, pane)
   local overrides = window:get_config_overrides() or {}
-  if not overrides.window_background_opacity then
-    overrides.window_background_opacity = 0.95
+  local current = overrides.window_background_image
+  if current == nil then
+    overrides.window_background_image = ''
+  elseif current == '' then
+    overrides.window_background_image = background_image
   else
-    overrides.window_background_opacity = nil
+    overrides.window_background_image = ''
   end
   window:set_config_overrides(overrides)
 end)
@@ -40,7 +49,7 @@ config.keys = {
   {
     key = '.',
     mods = 'CTRL',
-    action = wezterm.action.EmitEvent 'toggle-opacity',
+    action = wezterm.action.EmitEvent 'toggle-background',
   }
 }
 
