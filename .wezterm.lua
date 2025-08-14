ebb655a09f00c76d3af7af3685b4193567786346
Tyler Harding <tyler.harding.sde@gmail.com>
2025-08-14 07:31:48 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,10 +1,10 @@
 local wezterm = require 'wezterm'
 local mux = wezterm.mux
 
--- wezterm.on("gui-startup", function()
---   local tab, pane, window = mux.spawn_window{}
---   window:gui_window():maximize()
--- end)
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window{}
+  window:gui_window():maximize()
+end)
 
 local config = wezterm.config_builder()
 config.default_domain = 'WSL:Ubuntu'
@@ -20,16 +20,31 @@ config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 
 config.window_background_opacity = 1
-local background = 'C:\\Documents\\WezTerm\\TermBG\\02.jpg'
+local background = 'C:\\Documents\\WezTerm\\TermBG\\03.jpg'
+config.window_background_image = background
 
 wezterm.on('toggle-background', function(window, pane)
-    local overrides = window:get_config_overrides() or {}
-    if not overrides.window_background_image then
-        overrides.window_background_image = background
-    else
-        overrides.window_background_image = nil
+    if not config.window_background_image then
+      local overrides = window:get_config_overrides() or {}
+      if not overrides.window_background_image then
+          overrides.window_background_image = background
+      else
+          overrides.window_background_image = nil
+      end
+      window:set_config_overrides(overrides)
     end
-    window:set_config_overrides(overrides)
+    if config.window_background_image then
+      local overrides = window:get_config_overrides() or {}
+      local current = overrides.window_background_image
+      if current == nil then
+        overrides.window_background_image = ''
+      elseif current == '' then
+        overrides.window_background_image = background_image
+      else
+        overrides.window_background_image = ''
+      end
+      window:set_config_overrides(overrides)
+    end  
 end)
 
 wezterm.on('toggle-opacity', function(window, pane)
