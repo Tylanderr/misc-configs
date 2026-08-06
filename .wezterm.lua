3b83dd635869f443bbc35556d2850d9993dfa3d3
Tyler Harding <tyler.harding.sde@gmail.com>
2026-08-06 07:45:44 -0700

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,13 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
+
 local normal_font_size = 11
 local compact_font_size = 9.5
-local solid_bg = 1
-local opaque_bg = 0.95
+local solid_bg = 1.0
+local transparent_bg = 0.95
 
 local transparent_flag = true
+local background_image = 'C:\\Documents\\WezTerm\\TermBG\\08.jpg'
 
 config.default_domain = 'WSL:Ubuntu'
 config.check_for_updates = false
@@ -19,50 +21,45 @@ config.font = wezterm.font 'CaskaydiaMono Nerd Font Mono'
 config.window_decorations = 'RESIZE'
 config.window_close_confirmation = 'NeverPrompt'
 
--- config.window_background_image = 'C:\\Documents\\WezTerm\\TermBG\\03.jpg'
-
-if transparent_flag == true then
-  config.window_background_opacity = opaque_bg
+if transparent_flag then
+  config.window_background_image = ''
+  config.window_background_opacity = transparent_bg
 else
+  config.window_background_image = background_image
   config.window_background_opacity = solid_bg
 end
 
 wezterm.on('toggle-background', function(window, pane)
   local overrides = window:get_config_overrides() or {}
-  if config.window_background_image then
-    local current_image = overrides.window_background_image
-    if current_image == nil then
-      current_image = config.window_background_image
-    end
-    if current_image == '' then
-      overrides.window_background_image = config.window_background_image
-      overrides.window_background_opacity = nil
-    else
-      overrides.window_background_image = ''
-      overrides.window_background_opacity = opaque_bg
-    end
-  else
-    local current_opacity = overrides.window_background_opacity
-    if current_opacity == nil then
-      current_opacity = config.window_background_opacity
-    end
-    if current_opacity < solid_bg then
-      overrides.window_background_opacity = solid_bg
-    else
-      overrides.window_background_opacity = opaque_bg
-    end
+  local current_image = overrides.window_background_image
+
+  if current_image == nil then
+    current_image = config.window_background_image
   end
+
+  if current_image == '' then
+    -- Enable the image and disable transparency.
+    overrides.window_background_image = background_image
+    overrides.window_background_opacity = solid_bg
+  else
+    -- Disable the image and enable transparency.
+    overrides.window_background_image = ''
+    overrides.window_background_opacity = transparent_bg
+  end
+
   window:set_config_overrides(overrides)
 end)
 
 wezterm.on('font-size-switch', function(window, pane)
   local overrides = window:get_config_overrides() or {}
   local current_font_size = overrides.font_size or config.font_size
+
   if current_font_size == normal_font_size then
     overrides.font_size = compact_font_size
   else
     overrides.font_size = normal_font_size
   end
+
   window:set_config_overrides(overrides)
 end)
 
@@ -93,4 +90,5 @@ config.keys = {
     action = wezterm.action.EmitEvent 'font-size-switch',
   },
 }
+
 return config
